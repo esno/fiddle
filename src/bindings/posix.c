2a05874be817b43cf17f0151f2ae108176c4a223
Matthias Hauber <matthiashauber@googlemail.com>
2024-09-19 01:55:14 +0200

    posix: implement pthread_join and pthread_exit

--- src/bindings/posix.c
+++ src/bindings/posix.c
@@ -9,6 +9,8 @@
 #include "fiddle.h"
 #include "fiddle/lua.h"
 
+#define FDL_MTABLE_PTHREAD "_pthread_t"
+
 typedef struct fdl_threads fdl_threads;
 struct fdl_threads {
   pthread_t tid;
@@ -40,10 +42,37 @@ static int _fdl_pthread_create(lua_State *L) {
     return 3;
   }
 
+  luaL_getmetatable(L, FDL_MTABLE_PTHREAD);
+  lua_setmetatable(L, -2);
+
   return 1;
 }
 
-//-- rc, errno, strerrno = pthread_join(pthread);
+//-- pthread_exit(pthread);
+static int _fdl_pthread_exit(lua_State *L) {
+  fdl_threads *udata = (fdl_threads *) luaL_checkudata(L, 1, FDL_MTABLE_PTHREAD);
+  pthread_exit(NULL);
+
+  // check lua stack if we miss a fuction return
+  // due to pthread_exit(3)
+  return 0;
+}
+
+//-- bool, errno, strerrno = pthread_join(pthread);
+static int _fdl_pthread_join(lua_State *L) {
+  fdl_threads *udata = (fdl_threads *) luaL_checkudata(L, 1, FDL_MTABLE_PTHREAD);
+  int rc = pthread_join(udata->tid, NULL);
+
+  if (rc != 0) {
+    lua_pushnil(L);
+    lua_pushinteger(L, rc);
+    lua_pushstring(L, (const char *) strerrorname_np(rc));
+    return 3;
+  }
+
+  lua_pushboolean(L, 1);
+  return 1;
+}
 
 //-- rc, errno, strerrno = waitpid(pid)
 static int _fdl_waitpid(lua_State *L) {
@@ -63,9 +92,21 @@ static int _fdl_waitpid(lua_State *L) {
 void fdl_posix_luaopen(lua_State *L) {
   struct luaL_Reg lfuncs[] = {
     { "pthread_create", _fdl_pthread_create },
+    { "pthread_exit", _fdl_pthread_exit },
+    { "pthread_join", _fdl_pthread_join },
     { "waitpid", _fdl_waitpid },
     { NULL, NULL }
   };
-
 	fdl_lua_register_global(L, lfuncs);
+
+  struct luaL_Reg lfuncs_mtable[] = {
+    { "exit", _fdl_pthread_exit },
+    { "join", _fdl_pthread_join },
+    { NULL, NULL }
+  };
+  luaL_newmetatable(L, FDL_MTABLE_PTHREAD); {
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+    luaL_setfuncs(L, lfuncs_mtable, 0);
+  }
 }
