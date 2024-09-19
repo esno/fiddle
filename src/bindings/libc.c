769305c6d39541a2569736b39dadac9479a12ced
Matthias Hauber <matthiashauber@googlemail.com>
2024-09-19 19:07:44 +0200

    libc: implement fileno() and fdopen()

--- src/bindings/libc.c
+++ src/bindings/libc.c
@@ -9,24 +9,19 @@
 
 static int _fdl_fclose(lua_State *L);
 
-static luaL_Stream *_pushfile(lua_State *L) {
-  luaL_Stream *p = (luaL_Stream *) lua_newuserdata(L, sizeof(luaL_Stream));
-  p->closef = &_fdl_fclose;
-  luaL_setmetatable(L, LUA_FILEHANDLE);
-  return p;
-}
-
-//-- bool, errno, strerrno = close(stream)
-static int _fdl_fclose(lua_State *L) {
-  luaL_Stream *udata = (luaL_Stream *) luaL_checkudata(L, 1, LUA_FILEHANDLE);
-  if (fclose(udata->f) == -1) {
+//-- fd, errno, strerrno = dup2(oldfd, newfd)
+static int _fdl_dup2(lua_State *L) {
+  int oldfd = luaL_checkinteger(L, 1);
+  int newfd = luaL_checkinteger(L, 2);
+  int fd = dup2(oldfd, newfd);
+  if (fd != newfd) {
     lua_pushnil(L);
     lua_pushinteger(L, errno);
     lua_pushstring(L, (const char *) strerrorname_np(errno));
     return 3;
   }
 
-  lua_pushboolean(L, 1);
+  lua_pushinteger(L, fd);
   return 1;
 }
 
@@ -53,6 +48,56 @@ static int _fdl_execvp(lua_State *L) {
   return 1;
 }
 
+//-- bool, errno, strerrno = close(stream)
+static int _fdl_fclose(lua_State *L) {
+  luaL_Stream *udata = (luaL_Stream *) luaL_checkudata(L, 1, LUA_FILEHANDLE);
+  if (fclose(udata->f) == -1) {
+    lua_pushnil(L);
+    lua_pushinteger(L, errno);
+    lua_pushstring(L, (const char *) strerrorname_np(errno));
+    return 3;
+  }
+
+  lua_pushboolean(L, 1);
+  return 1;
+}
+
+//-- file = fdopen(fd)
+static int _fdl_fdopen(lua_State *L) {
+  int fd = luaL_checkinteger(L, 1);
+  const char *mode = luaL_checkstring(L, 2);
+
+  FILE *stream = fdopen(fd, mode);
+  if (fd == -1) {
+    lua_pushnil(L);
+    lua_pushinteger(L, errno);
+    lua_pushstring(L, (const char *) strerrorname_np(errno));
+    return 3;
+  }
+
+  luaL_Stream *p = (luaL_Stream *) lua_newuserdata(L, sizeof(luaL_Stream));
+  p->closef = &_fdl_fclose;
+  p->f = stream;
+  luaL_setmetatable(L, LUA_FILEHANDLE);
+  return 1;
+}
+
+//-- fd, errno, strerrno = fileno(stream)
+static int _fdl_fileno(lua_State *L) {
+  luaL_Stream *udata = (luaL_Stream *) luaL_checkudata(L, 1, LUA_FILEHANDLE);
+  int fd = fileno(udata->f);
+
+  if (fd == -1) {
+    lua_pushnil(L);
+    lua_pushinteger(L, errno);
+    lua_pushstring(L, (const char *) strerrorname_np(errno));
+    return 3;
+  }
+
+  lua_pushinteger(L, fd);
+  return 1;
+}
+
 //-- pid, errno, strerrno = fork()
 static int _fdl_fork(lua_State *L) {
   pid_t pid = fork();
@@ -67,22 +112,6 @@ static int _fdl_fork(lua_State *L) {
   return 1;
 }
 
-//-- fd, errno, strerrno = dup2(oldfd, newfd)
-static int _fdl_dup2(lua_State *L) {
-  luaL_Stream *oldfd = (luaL_Stream *) luaL_checkudata(L, 1, LUA_FILEHANDLE);
-  int newfd = luaL_checkinteger(L, 2);
-  int fd = dup2(fileno(oldfd->f), newfd);
-  if (fd != newfd) {
-    lua_pushnil(L);
-    lua_pushinteger(L, errno);
-    lua_pushstring(L, (const char *) strerrorname_np(errno));
-    return 3;
-  }
-
-  lua_pushinteger(L, fd);
-  return 1;
-}
-
 //-- rx, tx, errno, strerrno = pipe()
 static int _fdl_pipe(lua_State *L) {
   int fds[2];
@@ -94,20 +123,20 @@ static int _fdl_pipe(lua_State *L) {
     return 4;
   }
 
-  luaL_Stream *rx = _pushfile(L);
-  luaL_Stream *tx = _pushfile(L);
-  rx->f = fdopen(fds[0], "r");
-  tx->f = fdopen(fds[1], "w");
+  lua_pushinteger(L, fds[0]);
+  lua_pushinteger(L, fds[1]);
 
   return 2;
 }
 
 void fdl_libc_luaopen(lua_State *L) {
   struct luaL_Reg lfuncs[] = {
+    { "dup2", _fdl_dup2 },
     { "execvp", _fdl_execvp },
     { "fclose", _fdl_fclose },
+    { "fdopen", _fdl_fdopen },
+    { "fileno", _fdl_fileno },
     { "fork", _fdl_fork },
-    { "dup2", _fdl_dup2 },
     { "pipe", _fdl_pipe },
     { NULL, NULL }
   };
